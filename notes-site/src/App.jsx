137e05ef07f69155ff832e18c20620f4f0f6cce5
Einar Strandberg <einargs@gmail.com>
2023-10-08 18:13:56 -0500

    Got some progress on the audio

--- notes-site/src/App.jsx
+++ notes-site/src/App.jsx
@@ -1,20 +1,7 @@
+import { Button } from '@/components/ui/button'
 import { useState, useEffect } from 'react'
-import { socket, sendAudio } from './socket.js'
-
-function SendAudio() {
-  const [isLoading, setIsLoading] = useState(false)
-  function onSendAudio() {
-    setIsLoading(true)
-    sendAudio(() => {
-      setIsLoading(false)
-    })
-  }
-
-  return (
-    <button className="bg-indigo-500 text-white rounded text-lg px-2 py-1 shadow-md"
-      onClick={onSendAudio} disabled={isLoading}>Send Audio</button>
-  );
-}
+import { socket, useSocket } from './socket'
+import { SendAudio } from './SendAudio.jsx'
 
 function TranscriptLine({line}) {
   const isDoctor = line.startsWith("D:")
@@ -26,45 +13,58 @@ function TranscriptLine({line}) {
   )
 }
 
+function SummaryBox({summary}) {
+  return (
+    <div className="sticky top-20 basis-1/3 rounded bg-muted shadow-md mx-2 p-2">
+      <h2 className="text-2xl px-2 pb-2">Summary</h2>
+      <div className="rounded p-2 bg-background">
+        {summary ? summary : "No summary generated yet."}
+      </div>
+    </div>
+  )
+}
+
+function TranscriptBox({transcript}) {
+  return (
+    <div className="basis-2/3 w-full block">
+      <h2 className="text-2xl p-2">Transcript</h2>
+      <ul>
+        {transcript.map((line, index) =>
+          <TranscriptLine line={line} key={index} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+function Header({ isConnected }) {
+  function moreTranscript() {
+    socket.emit('send-transcript')
+  }
+  function resetTranscript() {
+    socket.emit('reset-transcript')
+  }
+  return (
+    <header className="sticky top-0 z-10 bg-background w-full flex flex-row p-4 items-center justify-between shadow-md">
+      <div className="flex flex-row items-baseline space-x-4">
+        <h1 className="text-4xl">Simple Prototype</h1>
+        <span className="block text-3xl">Connected: {isConnected ? "yes" : "no"}</span>
+      </div>
+      <div className="flex flex-row items-center space-x-4">
+        <Button onClick={resetTranscript}>Reset Transcript</Button>
+        <Button onClick={moreTranscript}>More Transcript</Button>
+        <SendAudio />
+      </div>
+    </header>
+  )
+}
+
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [summary, setSummary] = useState("")
-  const [transcript, setTranscript] = useState([])
-
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onTranscriptUpdate(update) {
-      setSummary(update.summary)
-      setTranscript(update.transcript)
-    }
-    
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('transcript-update', onTranscriptUpdate)
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('transcript-update', onTranscriptUpdate)
-    };
-  }, []);
+  const {isConnected, transcript, summary } = useSocket()
 
   return (
     <>
-      <header className="sticky top-0 z-10 bg-white w-full flex flex-row p-4 items-center justify-between shadow-md">
-        <div className="flex flex-row items-baseline space-x-4">
-          <h1 className="text-4xl">Simple Prototype</h1>
-          <span className="block text-3xl">Connected: {isConnected ? "yes" : "no"}</span>
-        </div>
-        <SendAudio />
-      </header>
+      <Header isConnected={isConnected} />
       <main className="p-4 space-y-4">
         <div className="flex flex-row items-center space-x-4">
           <p>
@@ -72,20 +72,8 @@ function App() {
           </p>
         </div>
         <div className="flex flex-row items-start">
-          <div className="basis-2/3 w-full block">
-            <h2 className="text-2xl p-2">Transcript</h2>
-            <ul>
-              {transcript.map((line, index) =>
-                <TranscriptLine line={line} key={index} />
-              )}
-            </ul>
-          </div>
-          <div className="sticky top-20 basis-1/3 rounded bg-slate-100 shadow-md mx-2 p-2">
-            <h2 className="text-2xl px-2 pb-2">Summary</h2>
-            <div className="rounded p-2 bg-white">
-              {summary ? summary : "No summary generated yet."}
-            </div>
-          </div>
+          <TranscriptBox transcript={transcript} />
+          <SummaryBox summary={summary} />
         </div>
       </main>
     </>
--- notes-site/src/socket.js
+++ notes-site/src/socket.js
@@ -1,10 +1,94 @@
+import { useState, useEffect } from 'react'
 import { io } from 'socket.io-client';
+import RecordRTC from 'recordrtc'
 
 const URL = "localhost:5000";
 
 export const socket = io(URL);
 
-// Currently just sends an empty event, because we have no audio.
-export function sendAudio(handler) {
-  socket.timeout(5000).emit('audio', handler)
+export function useAudio() {
+  const [isRecording, setIsRecording] = useState(false)
+  useEffect(() => {
+    if (isRecording) {
+      let recorder = null
+      navigator.mediaDevices.getUserMedia({
+        audio: true
+      }).then(stream => {
+        console.log("got stream", stream)
+        console.log("test", RecordRTC)
+        // Copied from:
+        // https://gist.github.com/savelee/a3a3f46f393e0e9ce0aa4318bde50b08
+        recorder = RecordRTC(stream, {
+          type: 'audio',
+          mimeType: 'audio/wav', // I don't think we can get mp3?
+          // If we set this as the sample rate, the audio files will play too
+          // fast for some reason. Default and the result is understandable.
+          // sample rate will probably be determined by speech to text.
+          //sampleRate: 44100,
+
+          // MediaStreamRecorder, StereoAudioRecorder, WebAssemblyRecorder
+          // CanvasRecorder, GifRecorder, WhammyRecorder
+          recorderType: RecordRTC.StereoAudioRecorder,
+
+          // Dialogflow / STT requires mono audio
+          numberOfAudioChannels: 1,
+
+          // get intervals based blobs
+          // value in milliseconds
+          // as you might not want to make detect calls every seconds
+          timeSlice: 5000,
+
+          // as soon as the stream is available
+          // The blob seems to be a discrete, complete file each time it's
+          // called. So it produces a single .wav file every 5 seconds.
+          ondataavailable(blob) {
+            console.log("Sending blob", blob)
+            socket.emit('audio-chunk', blob)
+          }
+        })
+        recorder.startRecording()
+      })
+      return () => {
+        if (recorder !== null) {
+          recorder.stopRecording(() => {
+            const file = recorder.getBlob()
+            socket.emit('audio-end', file)
+          })
+        }
+      }
+    }
+  }, [isRecording])
+  return [isRecording, setIsRecording]
+}
+
+export function useSocket() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [summary, setSummary] = useState("")
+  const [transcript, setTranscript] = useState([])
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onTranscriptUpdate(update) {
+      setSummary(update.summary)
+      setTranscript(update.transcript)
+    }
+    
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('transcript-update', onTranscriptUpdate)
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('transcript-update', onTranscriptUpdate)
+    };
+  }, []);
+  return { isConnected, summary, transcript }
 }
