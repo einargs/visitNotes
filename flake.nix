d046b2554419592340e2632ce9498e9b9855ec3a
Einar Strandberg <einargs@gmail.com>
2023-10-16 03:22:13 -0500

    adding poetry files so nix sees them

--- flake.nix
+++ flake.nix
@@ -3,35 +3,40 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   nixConfig = {
     bash-prompt = ''\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]dev-shell:\w]\$\[\033[0m\] '';
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, poetry2nix }: 
   let system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.permittedInsecurePackages = [
           "openssl-1.1.1v"
         ];
+        overlay = [ poetry2nix.overlay ];
       };
-      # We have to do this
-      # pkgs = nixpkgs.legacyPackages.${system};
+
+      backend-app = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+      };
   in {
     devShells.x86_64-linux.default = (pkgs.buildFHSUserEnv {
       name = "audio";
       targetPkgs = pkgs: with pkgs; [
-        vlc
-        # alsa-lib
-        chromium
+        # vlc
+        docker
+        alsa-lib
         python311Packages.virtualenv
         python311
         nodejs_20
         nodePackages.pnpm
         gcc
         openssl_1_1
-        # libuuid
+        libuuid
         autoconf
         binutils
         curl
@@ -42,24 +47,24 @@
         unzip
         util-linux
         wget
+        poetry
       ];
+
+        # export PS1='\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]dev-shell:\w]\$\[\033[0m\] '
+      profile = ''
+        source .venv/bin/activate
+      ''; # may add commandline call to dotenv to load from .env
+        # virtualenv .venv
+
       /*src = [
        ./flake.nix
        ./flake.lock
       ];*/
 
-      # shellHook = ''
       /*shellHook = ''
         virtualenv .venv
         source .venv/bin/activate
       '';*/
-      # ./secret_keys is a file exporting SPEECH_KEY and SPEECH_REGION
-      profile = ''
-        PS1='\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]dev-shell:\w]\$\[\033[0m\] '
-        virtualenv .venv
-        source .venv/bin/activate
-        source ./secret_keys
-      '';
 
       /*unpackPhase = ''
         for srcFile in $src; do
@@ -68,5 +73,23 @@
       '';*/ 
     }).env;
 
+    packages.x86_64-linux.default = with pkgs; pkgs.dockerTools.buildImage {
+      name = "visit-notes";
+      tag = "latest";
+      copyToRoot = buildEnv {
+        name = "image-root";
+        pathsToLink = [ "/bin" ];
+        paths = [
+          dockerTools.fakeNss
+          backend-app.dependencyEnv
+        ];
+      };
+      config = {
+        Cmd = [ "hypercorn" "app:asgi" "-b" "localhost:80" ];
+        ExposedPorts = {
+          "80/tcp" = {};
+        };
+      };
+    };
   };
 }
